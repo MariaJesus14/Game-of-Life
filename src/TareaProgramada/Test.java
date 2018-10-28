9629047459d6b23d7271c280cdf090aeae55c2a2
Usuario <Usuario@192.168.0.23>
2018-10-28 14:12:54 -0600

    commitTarea

---
+++ src/TareaProgramada/Test.java
@@ -0,0 +1,18 @@
+
+package TareaProgramada;
+
+import javax.swing.JOptionPane;
+//import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Test {
+    public static void main(String[] args) {
+       // ImageIcon II = new ImageIcon("C:\\Users\\Usuario\\Desktop\\descarga.png");
+      JOptionPane.showMessageDialog(null, "Bienvenid@" + "\n"  + "Juego de la vida" + "\n"+"Jerry, Maria, Roibin");
+      JOptionPane.showi
+    }
+    
+}
