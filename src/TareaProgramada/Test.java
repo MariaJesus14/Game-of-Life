4b3628e51e81ace1f53b5907064c75f0f4d2c696
Usuario <Usuario@192.168.0.23>
2018-10-28 15:52:44 -0600

    commitTarea

--- src/TareaProgramada/Test.java
+++ src/TareaProgramada/Test.java
@@ -2,7 +2,7 @@
 package TareaProgramada;
 
 import javax.swing.JOptionPane;
-//import javax.swing.ImageIcon;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -10,9 +10,24 @@ import javax.swing.JOptionPane;
  */
 public class Test {
     public static void main(String[] args) {
-       // ImageIcon II = new ImageIcon("C:\\Users\\Usuario\\Desktop\\descarga.png");
+       ImageIcon II = new ImageIcon("C:\\Users\\Usuario\\Desktop\\descarga.png");
+       /*String [] opciones=null;
+       opciones [0]="8x8";
+       opciones[1]="9x9";
+       opciones[2]="10x10";
+       opciones[3]="11x11";
+       opciones[4]="12x12";
+       opciones[5]="13x13";
+       opciones[6]="14x14";
+       opciones[7]="15x15";
+       opciones[8]="16x16";
+       opciones[9]="10x10";
+       opciones[10]="18x18";
+       opciones[11]="19x19";
+       opciones[12]="20x20";
+       
       JOptionPane.showMessageDialog(null, "Bienvenid@" + "\n"  + "Juego de la vida" + "\n"+"Jerry, Maria, Roibin");
-      JOptionPane.showi
+      
     }
     
 }
